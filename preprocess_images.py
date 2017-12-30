5ef2c703c3e0d603adb18c72f56879239016e774
Nicholas Ding <nicholasdsj@gmail.com>
2017-12-30 11:11:29 -0800

    Finished the first submission, the method of cropping images might be wrong. Only achieved 12% accuracy, it was obvious by looking at the submission.csv.

--- preprocess_images.py
+++ preprocess_images.py
@@ -1,9 +1,11 @@
 import math
 import os
+import random
 
 from PIL import Image
 
 train_folder = '/home/nicholas/Workspace/Resources/Camera/train'
+validation_folder = '/home/nicholas/Workspace/Resources/Camera/validation'
 
 def crop_split(filepath, grid_size=512):
     filename = os.path.basename(filepath)
@@ -28,9 +30,30 @@ def crop_split(filepath, grid_size=512):
         print('Saving', os.path.join(dirname, '.'.join([name, str(idx), ext])))
         crop.save(os.path.join(dirname, '.'.join([name, str(idx), ext])))
 
-# Walk into each directory and process the images
-for folder in os.listdir(train_folder):
-    for filename in os.listdir(os.path.join(train_folder, folder)):
-        print('Processing', folder, filename)
-        crop_split(os.path.join(train_folder, folder, filename))
-        os.remove(os.path.join(train_folder, folder, filename))
+def batch_crop():
+    # Walk into each directory and process the images
+    for folder in os.listdir(train_folder):
+        for filename in os.listdir(os.path.join(train_folder, folder)):
+            print('Processing', folder, filename)
+            crop_split(os.path.join(train_folder, folder, filename))
+            os.remove(os.path.join(train_folder, folder, filename))
+
+def generate_validation_set():
+    for folder in os.listdir(train_folder):
+        files = os.listdir(os.path.join(train_folder, folder))
+        validation_set = random.sample(files, k=int(len(files) * 0.2))
+
+        print('Moving %d files out of %d' % (len(validation_set), len(files)))
+
+        assert len(validation_set) == len(set(validation_set))
+
+        # Create target folder
+        target_folder = os.path.join(validation_folder, folder)
+        if not os.path.exists(target_folder):os.mkdir(target_folder)
+
+        # Move files
+        for filename in validation_set:
+            os.rename(os.path.join(train_folder, folder, filename), os.path.join(target_folder, filename))
+
+# batch_crop()
+generate_validation_set()
--- vanilla_cnn.py
+++ vanilla_cnn.py
@@ -6,7 +6,9 @@ import os
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Any results you write to the current directory are saved as output.
+import keras
+
+from PIL import Image
 
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, LSTM, BatchNormalization, Flatten
@@ -15,34 +17,44 @@ from keras.callbacks import ModelCheckpoint
 from keras import optimizers
 from keras.layers.normalization import BatchNormalization
 
-list_classes = ['Samsung-Galaxy-Note3', 'HTC-1-M7', 'Sony-NEX-7', 'Samsung-Galaxy-S4', 'iPhone-4s', 'Motorola-Nexus-6', 'LG-Nexus-5x', 'Motorola-X', 'iPhone-6', 'Motorola-Droid-Maxx']
+
+list_classes = ['HTC-1-M7', 'LG-Nexus-5x', 'Motorola-Droid-Maxx', 'Motorola-Nexus-6', 'Motorola-X', 'Samsung-Galaxy-Note3', 'Samsung-Galaxy-S4', 'Sony-NEX-7', 'iPhone-4s', 'iPhone-6']
 
 def build_model(num_classes, input_shape):
     model = Sequential()
     
     # Conv layers
     model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling2D((2, 2)))
     model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
     model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
     model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
     model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
 
     # Activation
     model.add(Flatten())
     model.add(Dropout(0.5))
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
+    model.add(BatchNormalization())
     model.add(Dense(num_classes, activation='softmax'))
     
     # Compile
     model.compile(optimizer=optimizers.RMSprop(lr=1e-4), 
-        loss='categorical_crossentropy', metrics=['acc'])
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
     
     model.summary()
 
@@ -60,15 +72,41 @@ def build_generator():
     
     return generator
 
+def train_model():
+    train_folder = '/home/nicholas/Workspace/Resources/Camera'
 
-train_folder = '/home/nicholas/Workspace/Resources/Camera'
+    epochs = 1000
+    batch_size = 16
 
-batch_size = 32
+    train_datagen = build_generator()
+    train_generator = train_datagen.flow_from_directory(os.path.join(train_folder, 'train'), target_size=(256, 256), batch_size=batch_size)
+    validation_generator = train_datagen.flow_from_directory(os.path.join(train_folder, 'validation'), target_size=(256, 256), batch_size=batch_size)
 
-train_datagen = build_generator()
-train_generator = train_datagen.flow_from_directory(os.path.join(train_folder, 'train'), target_size=(256, 256), batch_size=batch_size)
+    print(train_generator.class_indices)
 
-checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.camera.hdf5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.camera.hdf5', verbose=1, save_best_only=True)
 
-model = build_model(len(list_classes), (256, 256, 3))
-model.fit_generator(train_generator, steps_per_epoch=2000 // batch_size, epochs=10, callbacks=[checkpointer])
+    model = build_model(len(list_classes), (256, 256, 3))
+    model.fit_generator(train_generator,
+                steps_per_epoch=2000 // batch_size, epochs=epochs, 
+                validation_data=validation_generator,
+                validation_steps=800 // batch_size,
+                callbacks=[checkpointer])
+
+def predict():
+    test_folder = '/home/nicholas/Workspace/Resources/Camera/test'
+
+    model = build_model(len(list_classes), (256, 256, 3))
+    model.load_weights('saved_models/weights.best.camera.hdf5')
+
+    for filename in os.listdir(test_folder):
+        im = Image.open(os.path.join(test_folder, filename))
+        resized = im.resize((256, 256), Image.NORMAL)
+        arr = np.array(resized)
+        y_hat = model.predict(np.array([arr]))
+        print(filename,list_classes[y_hat[0].argmax()])
+        # print(filename, y_hat)
+
+
+# train_model()
+predict()
